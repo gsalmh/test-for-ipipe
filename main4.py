1bd385e1cfb1ae4b4c444d0c0bbd8047fcb67fb4
gsalmh <gsalmh@163.com>
2021-05-26 20:23:51 +0800

    on branch2 at 526 add class

---
+++ main4.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+class Test(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def get_name(self):
+        return self.name
+    
+
+    def get_age(self):
+        return self.age
+
+        
