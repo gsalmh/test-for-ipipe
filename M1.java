9e5a7fcd233881feab29602b5cd33186787040f9
lvzhiwang <996074344@qq.com>
2021-07-16 10:41:50 +0800

    Create M1.java

---
+++ M1.java
@@ -0,0 +1,26 @@
+public class M1 {
+    public void goodCase1() {
+        try {
+        } catch (IndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    public void goodCase2() {
+        try {
+        } catch (IndexOutOfBoundsException e) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+    public void badCase1() {
+        try {
+        } catch (IndexOutOfBoundsException e) {
+            throw new NullPointerException();
+        }
+    }
+    public void badCase2() {
+        try {
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+}
