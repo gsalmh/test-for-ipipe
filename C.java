a9a962474e79c975bdf4b59cba5512138e5bd6ec
daersanpao <lv996074344zhi@163.com>
2021-07-29 17:37:12 +0800

    Update C.java

--- C.java
+++ C.java
@@ -1,6 +1,7 @@
 public class C {
    
     public void badCase2() {
+       int a = 1/0;
         try {
         } catch (ArrayIndexOutOfBoundsException e) {
             throw new IndexOutOfBoundsException();
